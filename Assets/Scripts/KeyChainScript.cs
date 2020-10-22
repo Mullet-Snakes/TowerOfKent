642b5785d62e826544bacba884fd16707db78a94
Matthew Elmes <s200408@students.aie.edu.au>
2020-10-22 15:39:21 +1030

    Added KeyChain and KeyScript classes

---
+++ Assets/Scripts/KeyChainScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyChainScript
+{
+    static private Dictionary<string, bool> keyChain = new Dictionary<string, bool>();
+
+    static public void AddKey(string keyName)
+    {
+        if (keyName != null)
+        {
+            keyChain.Add(keyName, false);
+        }       
+    }
+
+    static public void PickUpKey(string keyName)
+    {
+        if(keyName != null)
+        {
+            keyChain[keyName] = true;
+        }
+    }
+
+    static public bool HasKey(string keyName)
+    {
+        bool hasKey = keyChain.ContainsKey(keyName) ? true : false;
+
+        return hasKey;
+
+    }
+
+    static public void PrintKeyChain()
+    {
+        foreach(KeyValuePair<string, bool> key in keyChain)
+        {
+            Debug.Log(key.Key);
+            Debug.Log(key.Value);
+        }
+    }
+}
---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+    public string keyName = "bluekey";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KeyChainScript.AddKey(keyName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            KeyChainScript.PickUpKey(keyName);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            KeyChainScript.PrintKeyChain();
+        }
+    }
+}
