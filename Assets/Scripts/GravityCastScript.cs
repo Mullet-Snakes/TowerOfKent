7b27a0ef99bb515a275c96364c4e2fc8044869ab
Matthew Elmes <s200408@students.aie.edu.au>
2020-10-22 11:48:20 +1030

    Initial push

---
+++ Assets/Scripts/GravityCastScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCastScript : MonoBehaviour
+{
+    private PlayerController playerControllerScript = null;
+
+    [Tooltip("List of walls")]
+    public List<GameObject> levelWalls;
+
+
+    [Tooltip("Highlighted wall material")]
+    public Material highlightedWallMaterial = null;
+
+    [Tooltip("Default wall material")]
+    public Material defaultWallMaterial = null;
+
+    private GravityController targeted = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = transform.parent.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(transform.position, transform.forward * 50, Color.red);
+
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, Mathf.Infinity))
+            {
+
+                hit.transform.GetComponent<Renderer>().material = highlightedWallMaterial;
+
+                foreach (GameObject go in levelWalls)
+                {
+                    if (go != hit.transform.gameObject)
+                    {
+                        go.GetComponent<Renderer>().material = defaultWallMaterial;
+                    }
+                }
+
+                playerControllerScript.Gravity = hit.normal;
+
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, Mathf.Infinity))
+            {
+                if (hit.transform.CompareTag("GravityWall"))
+                {
+                    GravityManager.ChangeGrav(hit.normal, false);
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, Mathf.Infinity))
+            {
+                if (hit.transform.GetComponent<GravityController>() != null)
+                {
+                    if(targeted != null)
+                    {
+                        targeted.IsTargeted = false;
+
+                        if(targeted.gameObject == hit.transform.gameObject)
+                        {
+                            targeted = null;
+                            return;
+                        }                     
+                    }
+
+                    targeted = hit.transform.GetComponent<GravityController>();
+                    targeted.IsTargeted = true;
+                }
+                if (hit.transform.CompareTag("GravityWall"))
+                {
+                    if (targeted != null)
+                    {
+                        GravityManager.ChangeGrav(hit.normal, true);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+
+    private Vector3 currentGravity = new Vector3();
+    private Rigidbody m_rb;
+    private bool isTargeted = false;
+
+    public bool IsTargeted
+    {
+        get
+        {
+            return isTargeted;
+        }
+        set
+        {
+            isTargeted = value;
+        }
+    }
+
+    public Material highlighed;
+    public Material normal;
+
+    [SerializeField]
+    [Tooltip("Default: -9.8")]
+    [Range(-30, 0)]
+    private float gravityFactor = -9.8f;
+
+    private void OnEnable()
+    {
+        GravityManager.GravityChange += SetCurrentGravity;
+    }
+
+    private void OnDisable()
+    {
+        GravityManager.GravityChange -= SetCurrentGravity;
+    }
+
+    private void Awake()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentGravity = GravityManager.worldGravity;
+    }
+
+    private void Update()
+    {
+        if (isTargeted)
+        {
+            transform.GetComponent<Renderer>().material = highlighed;
+        }
+        else
+        {
+            transform.GetComponent<Renderer>().material = normal;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_rb.AddForce(currentGravity * Time.deltaTime, ForceMode.VelocityChange);
+    }
+
+    void SetCurrentGravity(Vector3 grav, bool changingTargeted)
+    {
+
+        if(changingTargeted)
+        {
+            if(isTargeted)
+            {
+                currentGravity = grav * gravityFactor;
+            }
+        }
+        else
+        {
+            if(!isTargeted)
+            {
+                currentGravity = grav * gravityFactor;
+            }
+        }
+    }
+}
