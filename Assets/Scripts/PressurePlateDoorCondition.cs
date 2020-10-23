d0950d580564aefa5a3c6005b8fbd181547de5a7
Matthew Elmes <s200408@students.aie.edu.au>
2020-10-23 14:51:13 +1030

    Added PressurePlateDoorCondition class, and PressurePlateScript

---
+++ Assets/Scripts/PressurePlateDoorCondition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateDoorCondition : DoorConditionScript
+{
+    public List<GameObject> pressurePlateList = null;
+
+    override public bool CheckCondition()
+    {
+        foreach(GameObject plate in pressurePlateList)
+        {
+            if(plate.GetComponent<PressurePlateScript>() != null)
+            {
+                if(!plate.GetComponent<PressurePlateScript>().isColliding)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
