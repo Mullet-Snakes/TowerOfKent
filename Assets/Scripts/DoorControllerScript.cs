9d7050cb2d3752ed783f40808e0894fd48e1c0b0
Matthew Elmes <s200408@students.aie.edu.au>
2020-10-23 12:16:28 +1030

    Added logic to DoorControllerScript for opening and closing doors

--- Assets/Scripts/DoorControllerScript.cs
+++ Assets/Scripts/DoorControllerScript.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class DoorControllerScript : MonoBehaviour
 {
     public DoorConditionScript condition = null;
-    public bool open = false;
+    public bool canOpen = false;
+
+    public Transform left;
+    public Transform right;
+
+    public float timeToMove = 2f;
+
+    public float distanceToMove = 5f;
+
+    public bool running = false;
+    private bool open = false;
+    private bool closed = true;
 
     // Start is called before the first frame update
     void Start()
@@ -13,12 +24,60 @@ public class DoorControllerScript : MonoBehaviour
         
     }
 
+    IEnumerator MoveDoor(Transform door, float dist)
+    {
+        Vector3 startingPos = door.position;
+        Vector3 endPos =  new Vector3(door.position.x + dist, door.position.y, door.position.z);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < timeToMove)
+        {
+            running = true;
+            door.position = Vector3.Lerp(startingPos, endPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        running = false;
+    }
+
+    public void OpenDoor()
+    {
+        if(closed && !running)
+        {
+            StartCoroutine(MoveDoor(right, -distanceToMove));
+            StartCoroutine(MoveDoor(left, distanceToMove));
+            open = true;
+            closed = false;
+        }
+    }
+
+    public void CloseDoor()
+    {
+        if(open && !running)
+        {
+            StartCoroutine(MoveDoor(right, distanceToMove));
+            StartCoroutine(MoveDoor(left, -distanceToMove));
+            open = false;
+            closed = true;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.H))
+        if (Input.GetKeyDown(KeyCode.N))
         {
-            open = condition.CheckCondition();
+            OpenDoor();
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            CloseDoor();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            canOpen = condition.CheckCondition();
         }
     }
 }
