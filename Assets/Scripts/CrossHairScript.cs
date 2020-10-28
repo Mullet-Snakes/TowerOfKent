6b24c3443607db91af2b0954bff34501842cb5cf
Joel Lynch <miner49er5635@gmail.com>
2020-10-28 16:25:10 +1030

    Pause Menu Functionality

---
+++ Assets/Scripts/CrossHairScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHairScript : MonoBehaviour
+{
+
+    public GameObject CrossHairUI;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        while (PauseMenu.GameIsPaused)
+        {
+            CrossHairUI.SetActive(false);
+        }
+    }
+}
