4fd850c8eeff3ad1f7fac69b5cc1d846d7869cc7
Matthew Elmes <s200408@students.aie.edu.au>
2020-10-30 09:36:55 +1030

    Added singleton to InteractKeyManager

--- Assets/Scripts/InteractKeyManager.cs
+++ Assets/Scripts/InteractKeyManager.cs
@@ -9,8 +9,29 @@ public class InteractKeyManager : MonoBehaviour
     public KeyCode interactKey = KeyCode.E;
     private GameObject playerPosition = null;
 
+    private static InteractKeyManager _instance = null;
+    public static InteractKeyManager _Instance
+    {
+        get
+        {
+            return _instance;
+        }    
+    }
+
     private void Awake()
     {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
         playerPosition = GameObject.FindWithTag("Player");
     }
 
