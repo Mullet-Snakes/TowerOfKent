d7d68f4c79c793bf994440bac4f0357b15bdc7ef
Matthew Elmes <s200408@students.aie.edu.au>
2020-10-29 13:45:09 +1030

    ..

--- Assets/Scripts/LoadSceneCollider.cs
+++ Assets/Scripts/LoadSceneCollider.cs
@@ -7,9 +7,9 @@ public class LoadSceneCollider : MonoBehaviour
     public bool loaded = false;
     public string levelName = "";
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if(!loaded)
+        if (!loaded)
         {
             Utilities.LoadScene(levelName);
         }
