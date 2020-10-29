8e921af11cc24c5f36be1fa00a84f19d970fb168
Matthew Elmes <s200408@students.aie.edu.au>
2020-10-29 13:35:47 +1030

    Added temporary loadscene prefab

---
+++ Assets/Scripts/LoadSceneCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneCollider : MonoBehaviour
+{
+    private bool loaded = false;
+    public string levelName = "";
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!loaded)
+        {
+            Utilities.LoadScene(levelName);
+        }
+
+        loaded = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
