9e9baa0b150b93d8976af36ba9c4801df53c4c70
J0laN5635 <miner49er5635@gmail.com>
2020-10-29 13:28:52 +1030

    Updated Main Menu
    Condensed the Main Menu Scripts into a single script.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Starts New Game
+    public void New()
+    {
+        Debug.Log("Starting New Game");
+        SceneManager.LoadScene("Greybox TUT Level");
+    }
+
+    //Loads Save Game
+    public void LoadGame()
+    {
+        Debug.Log("Loading Save Game");
+    }
+
+    //Exits Game
+    public void Exit()
+    {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    }
+
+    //Loads Settings Menu
+    public void SettingsMenu()
+    {
+        Debug.Log("Loading Settings Menu");
+    }
+}
+
+
