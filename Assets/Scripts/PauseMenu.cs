a9bc58b9bd3c1415a847845a35eb77be9ffbb0ac
J0laN5635 <miner49er5635@gmail.com>
2020-10-29 13:16:11 +1030

    Pause Menu
    Fully Functioning Pause Menu, Pause Button Still TAB

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
     public GameObject PauseMenuUI;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,26 +33,33 @@ public class PauseMenu : MonoBehaviour
 
     void Pause()
     {
+        Debug.Log("Game Paused");
         PauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         GameIsPaused = true;
     }
 
     public void Resume()
     {
+        Debug.Log("Game Resumed");
         PauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         GameIsPaused = false;
     }
 
     public void SettingsMenu()
     {
-
+        Debug.Log("Loading Settings Menu");
     }
 
     public void ExitToMainMenu()
     {
-
+        Debug.Log("Returning to Main Menu");
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void Save()
