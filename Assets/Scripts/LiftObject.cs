3d62c0cc2a935c67c876117402afa3b505d3f1f8
Dylan White <s191796@students.aie.edu.au>
2020-11-05 16:30:52 +1030

    Added broken collisions

--- Assets/Scripts/LiftObject.cs
+++ Assets/Scripts/LiftObject.cs
@@ -4,11 +4,65 @@ using UnityEngine;
 
 public class LiftObject : MonoBehaviour
 {
-    private bool isHolding = false;
+    #region failed code
+    //public Transform destination;
+    //private Camera playerCamera;
+
+    //void Awake()
+    //{
+    //    playerCamera = Camera.main;
+    //}
+
+    //private void Updat()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.I) && !grabbed)
+    //    {
+    //        Vector3 rayOrigin = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+    //        RaycastHit hit;
+
+    //        if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, Mathf.Infinity))
+    //        {
+    //            if (hit.collider.CompareTag("LevelProp"))
+    //            {
+    //                grabbed = true;
+    //                Rigidbody prop_rb = this.GetComponent<Rigidbody>();
+
+    //                while (grabbed == true)
+    //                {
+    //                    //GetComponent<BoxCollider>().enabled = false;
+    //                    hit.collider.GetComponent<GravityController>().GravityFactor = 0f;
+    //                    this.transform.position = destination.position;
+
+    //                    //Transform propTransform = prop_rb.transform;
+    //                    //float elapsedTime = 0f;
+    //                    //prop_rb.MovePosition(Vector3.Lerp(propTransform.position, destination.position, (elapsedTime / propMove)));
+    //                    //elapsedTime += Time.deltaTime;
+
+    //                    prop_rb.transform.parent = destination;
+
+    //                    if (Input.GetKeyDown(KeyCode.O) && grabbed == true)
+    //                    {
+    //                        grabbed = false;
+    //                        this.transform.parent = null;
+    //                        this.GetComponent<GravityController>().GravityFactor = -9.8f;
+    //                        //GetComponent<BoxCollider>().enabled = true;
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+
+    //}
+    #endregion
+
+    //public bool canHold = true;
+    public GameObject player;
     private GameObject carriedObject;
     private GameObject mainCamera;
     private Camera playerCamera;
     private Rigidbody prop_rb;
+    //private Vector3 objectPos;
+    private Vector3 noGrav = new Vector3(0, 0, 0);
 
     [SerializeField]
     [Tooltip("The movement of the object you're holding (default 20)")]
@@ -30,6 +84,69 @@ public class LiftObject : MonoBehaviour
     [Tooltip("The distance between an object you're holding and you")]
     [Range(0, 5)]
     public float holdDistance;
+
+    private bool isHolding = false;
+    //private float elapsedTime = 0f;
+
+    #region Bad Code
+    //// Update is called once per frame
+    //void pdate()
+    //{
+    //    distance = Vector3.Distance(levelProp.transform.position, tempParent.transform.position);
+    //    grabDistance = player.GetComponent<PlayerController>().playerGrab;
+
+    //    if (distance >= grabDistance)
+    //    {
+    //        elapsedTime = 0;
+    //        isHolding = false;
+    //    }
+
+    //    //Check if isholding
+    //    if (isHolding == true)
+    //    {
+    //        prop_rb = levelProp.GetComponent<Rigidbody>();
+    //        Vector3 propPosition = prop_rb.position;
+
+    //        prop_rb.velocity = Vector3.zero;
+    //        prop_rb.angularVelocity = Vector3.zero;
+    //        //levelProp.transform.position = grabby.transform.position;
+    //        //levelProp.transform.rotation = grabby.transform.rotation;
+    //        levelProp.transform.SetParent(tempParent.transform);
+    //        //prop_rb.MovePosition(Vector3.Lerp(propPosition, tempParent.transform.position, (elapsedTime / moveTime)));
+    //        elapsedTime += Time.deltaTime;
+
+    //        if (Input.GetMouseButtonDown(1))
+    //        {
+    //            elapsedTime = 0;
+    //            levelProp.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
+    //            isHolding = false;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        elapsedTime = 0;
+    //        objectPos = levelProp.transform.position;
+    //        levelProp.transform.SetParent(null);
+    //        this.GetComponent<GravityController>().CurrentGravity = player.GetComponent<PlayerController>().Gravity;
+    //        levelProp.transform.position = objectPos;
+    //    }
+    //}
+
+    //void OnMouseDown()
+    //{
+    //    if (distance <= grabDistance)
+    //    {
+    //        isHolding = true;
+    //        this.GetComponent<GravityController>().CurrentGravity = noGrav;
+    //        levelProp.GetComponent<Rigidbody>().detectCollisions = true;
+    //    }
+    //}
+    //void OnMouseUp()
+    //{
+    //    isHolding = false;
+    //    elapsedTime = 0;
+    //}
+    #endregion
 
     private void Awake()
     {
@@ -59,6 +176,7 @@ public class LiftObject : MonoBehaviour
 
     void Carry(GameObject obj)
     {
+        //moveTime may be wrong
         obj.transform.position = Vector3.Lerp(obj.transform.position, mainCamera.transform.position + mainCamera.transform.forward * holdDistance, Time.deltaTime * objMove);
     }
 
@@ -80,7 +198,8 @@ public class LiftObject : MonoBehaviour
                 {
                     isHolding = true;
                     carriedObject = prop_rb.gameObject;
-                    prop_rb.isKinematic = true;
+                    hit.collider.GetComponent<GravityController>().CurrentGravity = noGrav;
+                    //prop_rb.isKinematic = true;
                 }
             }
         }
@@ -97,7 +216,8 @@ public class LiftObject : MonoBehaviour
     void DropObject()
     {
         isHolding = false;
-        prop_rb.isKinematic = false;
+        prop_rb.GetComponent<GravityController>().CurrentGravity = player.GetComponent<PlayerController>().Gravity;
+        //prop_rb.isKinematic = false;
         prop_rb = null;
         carriedObject = null;
     }
