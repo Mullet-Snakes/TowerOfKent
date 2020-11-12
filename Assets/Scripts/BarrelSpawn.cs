a72fb67aeccd11766d5b5a235a2b3fe812f03ebe
Dylan White <dylanhwite@Gmail.com>
2020-11-12 15:27:59 +1030

    Added barrelSpawn and LiftObject

---
+++ Assets/Scripts/BarrelSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelSpawn : MonoBehaviour
+{
+    public GameObject barrelPrefab;
+
+    private void Start()
+    {
+        //barrel = barrelPrefab;
+        Instantiate(barrelPrefab, this.transform.position, this.transform.rotation);
+    }
+
+    public void Explode()
+    {
+        //barrel = barrelPrefab;
+        Instantiate(barrelPrefab, this.transform.position, this.transform.rotation);
+    }
+}
