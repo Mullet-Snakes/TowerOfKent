8bb17eeacd6217424dd4b6f616c95f5c23c67c77
Matthew Elmes <s200408@students.aie.edu.au>
2020-11-12 16:11:05 +1030

    ...

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject itemToSpawn = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            Spawn(transform.position, transform.rotation);
+        }
+    }
+
+    public void Spawn(Vector3 pos, Quaternion rot)
+    {
+        Instantiate(itemToSpawn, pos, rot);
+    }
+}
