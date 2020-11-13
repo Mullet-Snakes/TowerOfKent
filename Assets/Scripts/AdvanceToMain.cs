6b0e29782731aa1cd9bb854fee0e939702c9a32b
Joel Lynch <s181628@students.aie.edu.au>
2020-11-13 10:50:21 +1030

    Fade Transitions Between Title, Main Menu and Tutorial

--- Assets/Scripts/AdvanceToMain.cs
+++ Assets/Scripts/AdvanceToMain.cs
@@ -5,14 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class AdvanceToMain : MonoBehaviour
 {
-    //hey
+    public Animator transition;
+
+    public float transitionTime = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        int index = SceneManager.GetActiveScene().buildIndex;
+        
+        if(index == 0)
         {
-            SceneManager.LoadScene("MainMenu");
+            if (Input.anyKey)
+            {
+                LoadNextLevel();
+            }
         }
+        
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadMenu(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void startTutorial()
+    {
+        StartCoroutine(LoadTutorial());
+    }
+
+    IEnumerator LoadMenu(int levelIndex)
+    {
+        //Play Animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load Scene
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    IEnumerator LoadTutorial()
+    {
+        //Play Animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load Scene
+        SceneManager.LoadScene("Greybox TUT Level");
     }
 }
