65b0f6f48ef42ad67d80e1179612f094ab791b51
MJElmes <s200408@students.aie.edu.au>
2020-11-25 20:03:00 +1030

    ...

---
+++ Assets/Scripts/ModifyMaterialScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyMaterialScript : MonoBehaviour
+{
+    private Renderer m_mesh = null;
+
+    public float tilingRatio;
+ 
+
+
+    private void Awake()
+    {
+        m_mesh = GetComponent<Renderer>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 tilingAmount = new Vector2(transform.localScale.x, transform.localScale.z);
+        m_mesh.material.mainTextureScale = tilingAmount / tilingRatio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
